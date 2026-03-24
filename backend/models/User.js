c4589d3134522c0377c2f4d8f77b29ba84ff1158
vyku8888 <vyku8888@gmail.com>
2026-03-25 00:33:49 +0530

    Critical Bugfix: Repaired Mongoose hanging Save hook preventing password resets

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,10 +15,13 @@ const userSchema = new mongoose.Schema({
   resetPasswordExpire: Date
 }, { timestamps: true });
 
-userSchema.pre('save', async function() {
-  if (!this.isModified('password')) return;
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 userSchema.methods.matchPassword = async function(enteredPassword) {
